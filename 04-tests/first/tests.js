383fa28721223ca63f337db89033afdf1cd8f2c7
jp <jptick@gmail.com>
2020-10-24 15:19:17 -0300

    Tests using mocha

---
+++ 04-tests/first/tests.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const { 
+  obterPessoas 
+} = require('./service')
+
+const nock = require('nock')
+
+describe('Star Wars Tests', function() {  
+  it('deve buscar o r2d2 com o formato correto', async() =>{
+    const expected = [{
+      nome:'R2-D2',
+      peso:96
+    }]
+    const nomeBase = `r2-d2`
+    const resultado = await obterPessoas(nomeBase)
+    assert.deepEqual(resultado, expected)
+  })
+})
