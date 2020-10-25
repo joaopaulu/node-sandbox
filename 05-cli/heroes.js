44a9dfee58d4476a07eb848c97fe5d38c50631d2
jp <jptick@gmail.com>
2020-10-25 18:18:12 -0300

    Read file Handler

---
+++ 05-cli/heroes.js
@@ -0,0 +1,22 @@
+[
+{
+  "nome":"Flash",
+  "poder": "Velocidade",
+  "id":1
+},
+{
+  "nome":"Super Man",
+  "poder": "Força",
+  "id":2
+},
+{
+  "nome":"Batman",
+  "poder": "Inteligência",
+  "id":3
+},
+{
+  "nome":"Mulher Maravilha",
+  "poder": "Força",
+  "id":4
+}
+]
